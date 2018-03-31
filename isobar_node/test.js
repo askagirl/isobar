0ed23692ffc46cda8eebe7f681d7017b671d80db
HashimotoYT <hashimoto.stream@gmail.com>
2018-04-01 02:47:36 +0300

    Add napi support to monorepo and clean up

--- isobar_node/test.js
+++ isobar_node/test.js
@@ -1,4 +1,4 @@
-const {TextBuffer, TextEditor} = require('./target/debug/isobar.node')
+const {TextBuffer, TextEditor} = require('./target/debug/isobar_node')
 
 const buffer = new TextBuffer(1)
 buffer.splice(0, 0, 'Hello, world!')
