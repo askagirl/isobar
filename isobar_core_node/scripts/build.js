5958789189ce44c98c93838d26d26d414cdee2c7
HashimotoYT <hashimoto.stream@gmail.com>
2018-04-01 02:21:08 +0300

    Fix moduleName

--- isobar_core_node/scripts/build.js
+++ isobar_core_node/scripts/build.js
@@ -6,7 +6,7 @@ const path = require('path')
 const buildDebug = ['-d', '--debug'].includes(process.argv[2])
 const releaseFlag = buildDebug ? '' : '--relesae '
 const targetDir = buildDebug ? 'debug' : 'release'
-const moduleName = 'isobar_core';
+const moduleName = 'isobar';
 
 cp.execSync(`cargo rustc ${releaseFlag}--verbose -- -Clink-args=\"undefined dynamic_lookup -export_dynamic\"`, {stdio: 'inherit'})
 cp.execSync(`cp target/${targetDir}/{lib${moduleName}.dylib,${moduleName}.node}`, {stdio: 'inherit'})
