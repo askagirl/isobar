5835c540dcb13603bd22ddaa03d93b0e357d7144
HashimotoYT <hashimoto.stream@gmail.com>
2018-04-06 00:21:28 +0300

    Clean up script

--- napi/scripts/napi.js
+++ napi/scripts/napi.js
@@ -20,40 +20,39 @@ const argv = parseArgs(process.argv.slice(2), {
 const subcommand = argv._[0] || 'build'
 
 const nodeIncludePath = path.join(process.argv[0], '..', '..', 'include', 'node')
-const moduleName = path.basename(process.cwd());
+const moduleName = path.basename(process.cwd())
 process.env.NODE_INCLUDE_PATH = nodeIncludePath
 process.env.NODE_MAJOR_VERSION = nodeMajorVersion
 
 const platform = os.platform()
-var libExt = ''
-var platfromArgs = ''
+let libExt, platformArgs
 
 // Platform based massaging for build commands
-switch(platform) {
-      case 'darwin':
-              libExt = '.dylib'
-              platfromArgs = '-undefined dynamic_lookup -export_dynamic'
-              break;
-      case 'win32':
-              libExt = '.dll'
-              platfromArgs = '-undefined dynamic_lookup -export_dynamic'
-              break;
-      case 'linux':
-              libExt = '.so'
-              platfromArgs = '-undefined=dynamic_lookup -export_dynamic'
-              break;
-      default:
-              console.error('Operating system not currently supported or recognized by the build script')
-              process.exit(1)
+switch (platform) {
+  case 'darwin':
+    libExt = '.dylib'
+    platformArgs = '-undefined dynamic_lookup -export_dynamic'
+    break
+  case 'win32':
+    libExt = '.dll'
+    platformArgs = '-undefined dynamic_lookup -export_dynamic'
+    break
+  case 'linux':
+    libExt = '.so'
+    platformArgs = '-undefined=dynamic_lookup -export_dynamic'
+    break
+  default:
+    console.error('Operating system not currently supported or recognized by the build script')
+    process.exit(1)
 }
 
 switch (subcommand) {
   case 'build':
     const releaseFlag = argv.release ? '--release' : ''
     const targetDir = argv.release ? 'release' : 'debug'
-    cp.execSync(`cargo rustc ${releaseFlag} -- -Clink-args=\"${platfromArgs}\"`, {stdio: 'inherit'})
+    cp.execSync(`cargo rustc ${releaseFlag} -- -Clink-args=\"${platformArgs}\"`, {stdio: 'inherit'})
     cp.execSync(`cp target/${targetDir}/{lib${moduleName}.dylib,${moduleName}${libExt} target/${targetDir}/${moduleName}.node}`, {stdio: 'inherit'})
-    break;
+    break
   case 'check':
     cp.execSync(`cargo check`, {stdio: 'inherit'})
   case 'doc':
