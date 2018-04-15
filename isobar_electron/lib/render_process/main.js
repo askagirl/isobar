eb0515d0fcfeab5cdd8d3c3eaf9f73019b2fa7a8
HashimotoYT <hashimoto.stream@gmail.com>
2018-04-15 18:00:35 +0300

    Start work on connecting to socket from renderer

--- isobar_electron/lib/render_process/main.js
+++ isobar_electron/lib/render_process/main.js
@@ -1,15 +1,43 @@
 process.env.NODE_ENV = "production"
 
 const fs = require("fs");
+const QueryString = require("querystring");
 const path = require("path");
 const isobar = require("isobar");
 const React = require("react");
 const ReactDOM = require("react-dom");
 const Styletron = require("styletron-client");
-const { StyletronProvider } = require("styletron-react")
+const { StyletronProvider } = require("styletron-react");
+const IsobarClient = require('../shared/isobar_client');
 
 const ThemeProvider = require("./theme_provider");
 const TextEditor = require("./text_editor/text_editor");
+
+const {socketPath, windowId} = QueryString.parse(window.location.serach);
+
+const isobarClient = new IsobarClient();
+isobarClient.start(socketPath).then(() => {
+  console.log('started!!!');
+
+  isobarClient.addMessageListener(message => {
+    console.log("MESSAGE", message);
+  });
+
+  isobarClient.sendMessage({
+    type: 'StartWindow',
+    window_id: windowId
+  });
+
+  setInterval(() => {
+    isobarClient.sendMessage({
+      type: 'Action',
+      view_id: 0,
+      action: {
+        type: 'ToggleFileFinder'
+      }
+    });
+  }, 1000);
+});
 
 const $ = React.createElement;
 
