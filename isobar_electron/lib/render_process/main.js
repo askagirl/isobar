b1dd388eefbe246f45de4dbada4a5f7a1ea70748
Josh Cooper <ugtstreamer@ex.ua>
2018-05-25 11:56:57 +0300

    Export only App and buildViewRegistry from isobar_web

--- isobar_electron/lib/render_process/main.js
+++ isobar_electron/lib/render_process/main.js
@@ -1,14 +1,10 @@
 process.env.NODE_ENV = "production";
 
-const App = require("isobar_web/lib/app");
-const FileFinder = require("isobar_web/lib/file_finder");
+const { App, buildViewRegistry } = require("isobar_web");
+const IsobarClient = require("../shared/isobar_client");
 const QueryString = require("querystring");
 const React = require("react");
 const ReactDOM = require("react-dom");
-const ViewRegistry = require("isobar_web/lib/view_registry");
-const Workspace= require("isobar_web/lib/workspace");
-const TextEditorView = require("isobar_web/lib/text_editor/text_editor");
-const IsobarClient = require("../shared/isobar_client");
 const $ = React.createElement;
 
 async function start() {
@@ -42,19 +38,6 @@ async function start() {
     window_id: Number(windowId),
     height: window.innerHeight
   });
-}
-
-function buildViewRegistry(client) {
-  const viewRegistry = new ViewRegistry({
-    onAction: action => {
-      action.type = "Action";
-      client.sendMessage(action);
-    }
-  });
-  viewRegistry.addComponent("Workspace", Workspace);
-  viewRegistry.addComponent("FileFinder", FileFinder);
-  viewRegistry.addComponent("BufferView", TextEditorView);
-  return ViewRegistry;
 }
 
 start();
