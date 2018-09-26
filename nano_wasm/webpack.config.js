b4e0056309e91b0b09e4447aa694ef36af453e2e
HashimotoYT <hashimoto.stream@gmail.com>
2018-09-26 15:47:12 +0300

    Improve testing setup

---
+++ nano_wasm/webpack.config.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: "nano",
+    libraryTarget: "umd"
+  },
+  target: "node"
+};
