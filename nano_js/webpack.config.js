fd9e773f07e7701374e6cfcbc681f48da8ad86f3
Josh Cooper <ugtstreamer@ex.ua>
2018-10-22 00:13:59 +0300

    Revert "Convert tests to TypeScript"
    This reverts commit 2f9382b09b17d6460b5eed9ec117d405d1057b7b.

--- nano_js/webpack.config.js
+++ nano_js/webpack.config.js
@@ -1,6 +1,7 @@
 var path = require("path");
 
-const baseConfig = {
+module.exports = {
+  entry: "./src/index.ts",
   target: "node",
   module: {
     rules: [
@@ -14,36 +15,10 @@ const baseConfig = {
   resolve: {
     extensions: [".ts", ".js", ".wasm"]
   },
-};
-
-const libConfig = {
-  ...baseConfig,
-  entry: "./src/index.ts",
-  mode: "production",
-  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.node.js",
     library: "nano",
-    libraryTarget: "commonjs"
-  }
-};
-
-const testConfig = {
-  ...baseConfig,
-  entry: "./test/tests.ts",
-  mode: "development",
-  devtool: "cheap-eval-source-map",
-  output: {
-    path: path.resolve(__dirname, "test", "dist"),
-    filename: "tests.js"
-  }
-};
-
-module.exports = env => {
-  if (env && env.test) {
-    return testConfig;
-  } else {
-    return libConfig;
+    libraryTarget: "umd"
   }
 };
